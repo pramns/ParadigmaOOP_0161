4572973ce0ad2515cbc173273e532645e047a8d3
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-20 14:18:36 +0700

    create class with 4 member class

--- introOOP.cpp
+++ introOOP.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+  public : 
+    int nim;
+    string nama;
+    float nilai;
+
+    void print(){
+      cout << "Nim = " << nim << endl;
+      cout << "Nama = " << nama << endl;
+      cout << "Nilai =" << nilai << endl;
+
+    }
+};

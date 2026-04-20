91fd850641db320e94257a64f12ed3afb343bc45
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-20 14:48:19 +0700

    create class barang

--- OOP2.cpp
+++ OOP2.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class Barang { 
+  public :
+    string nama;
+    int jumlah;
+    string kategori;
+    string tanggalpro;
+
+    void Tampilkan(){
+      cout << "Nama = " << nama << endl;
+      cout << "jumlah = " << jumlah << endl;
+      cout << "Kategori = " << kategori << endl;
+      cout << "Tanggal Produksi = " << tanggalpro << endl;
+    }
+};
+
